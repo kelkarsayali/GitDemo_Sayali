e00d772c44cf2f133864af587a63f7bcfee52327
sayali <kelkarsayali92@gmail.com>
2024-06-12 13:58:56 +0530

    Architect

--- Introduction/src/POC_NatureFit/RestAPITest.java
+++ Introduction/src/POC_NatureFit/RestAPITest.java
@@ -14,5 +14,25 @@ public class RestAPITest {
 		System.out.println("postJira 4");
 		System.out.println("postJira 5");
 		System.out.println("postJira 6");
+		
+		//branch 1
+		System.out.println("postJira 7");
+		System.out.println("postJira 8");
+		System.out.println("postJira 9");
+		
 	}
+	
+	
+	@Test
+	public void chnages()
+	{
+				
+		//branch 1
+		System.out.println("postJira 10");
+		System.out.println("postJira 11");
+		System.out.println("postJira 12");
+		
+	}
+	
+	
 }
